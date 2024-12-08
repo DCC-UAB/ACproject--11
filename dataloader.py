d471c5e7735079e0fe491b590060e6d8cf7494f1
LuciaGarrido1 <lucia070704@gmail.com>
2024-12-08 12:34:40 +0100

    ultima actualitzacio

--- dataloader.py
+++ dataloader.py
@@ -13,9 +13,12 @@ def load_data(file_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     df = pd.read_csv(file_path) # Carreguem les dades del fitxer CSV
 
     # Per columnes amb valors binaris, fem un map per convertir-los a 0 i 1
+    
     df["gender"] = df["gender"].map({"Male": 1, "Female": 0})
     df["Residence_type"] = df["Residence_type"].map({"Urban": 1, "Rural": 0})
     df["ever_married"] = df["ever_married"].map({"Yes": 1, "No": 0})
+        
+    df['obesity'] = df['bmi'].map(lambda x: 1 if x >= 30 else 0)
 
     # Per columnes categoriques, fem un one-hot encoding
     df = pd.get_dummies(df, columns=["smoking_status", "work_type"])
